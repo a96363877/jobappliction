dd133a20c3516f73d15ea43dc993a7cb82cb0d17
a96363877 <a96363877@gmail.com>
2025-05-04 23:53:34 +0300

    as

--- components/job-application-form.tsx
+++ components/job-application-form.tsx
@@ -493,34 +493,7 @@ export default function JobApplicationForm() {
                           </FormItem>
                         )}
                       />
-                      <FormField
-                        control={form.control}
-                        name="dateOfBirth"
-                        render={({ field }) => (
-                          <FormItem className="flex flex-col">
-                            <FormLabel className="text-slate-700">Date of Birth</FormLabel>
-                            <Popover>
-                              <PopoverTrigger asChild>
-                                <FormControl>
-                                  <Button
-                                    variant={"outline"}
-                                    className={`w-full pl-3 text-left font-normal border-slate-300 ${
-                                      !field.value ? "text-muted-foreground" : ""
-                                    }`}
-                                  >
-                                    {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                  </Button>
-                                </FormControl>
-                              </PopoverTrigger>
-                              <PopoverContent className="w-auto p-0" align="start">
-                               <Calendar/>
-                              </PopoverContent>
-                            </Popover>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
+                     
                     </div>
                   </div>
                 </div>

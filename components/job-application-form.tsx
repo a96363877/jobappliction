796a5fcede8e6603eaee8c144759603937b5e737
a96363877 <a96363877@gmail.com>
2025-05-04 20:24:38 +0300

    sda

--- components/job-application-form.tsx
+++ components/job-application-form.tsx
@@ -723,7 +723,6 @@ export default function JobApplicationForm() {
                               </PopoverTrigger>
                               <PopoverContent className="w-auto p-0" align="start">
                                 <Calendar
-                                  mode="single"
                                   selected={field.value}
                                   onSelect={field!.onChange}
                                   disabled={(date: any) => date < new Date()}
